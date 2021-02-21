a43c90bc8bc5a29c95b5a12a236043900f2f1139
Michael Descombes <michael.descombes@grenoble-inp.org>
2021-02-21 16:37:49 +0100

    Resolved the 'name too long' problem in Adding Players. If a player's name was too long, it would display entirely and be quite ugly.

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Button, Col, ListGroup, Row } from 'react-bootstrap'
 
+import '../css/PlayerList.css'
+
 function PlayerList({ players, handleDeletePlayer }) {
   let listOfPlayers = players.map((player) => {
     return (
       <ListGroup.Item key={player}>
         <Row>
           <Col sm={10} xs={10}>
-            <span>{player}</span>
+            <span className="player-name">{player}</span>
           </Col>
           <Col sm={1} xs={1}>
             <Button size="sm" variant="light" onClick={(event) => handleDeletePlayer(event, player)}>X</Button>
