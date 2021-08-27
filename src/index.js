1d773924b7292de70ceb8d99dffe4d0ec06c0641
descomic <>
2021-08-27 16:00:17 +0200

    Add a warning message when reloading
    Now when the user will try to reload the app, a warning message,
    will prompt them that the games data might be lost.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import App from './components/App';
 // Adding bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+window.onbeforeunload = () => {
+  return "You will lose all your game data.\nThis app does not support cookies"
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
